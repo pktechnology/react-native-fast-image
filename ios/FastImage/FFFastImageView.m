6fa9a91670d732b67e6b557a4005e0edcec06926
trenton05 <tralbrecht@gmail.com>
2020-12-04 11:51:16 -0600

    Update FFFastImageView.m

--- ios/FastImage/FFFastImageView.m
+++ ios/FastImage/FFFastImageView.m
@@ -31,27 +31,27 @@
 
 - (void)setOnFastImageLoadEnd:(RCTDirectEventBlock)onFastImageLoadEnd {
     _onFastImageLoadEnd = onFastImageLoadEnd;
-    if (self.hasCompleted) {
+    if (self.hasCompleted && _onFastImageLoadEnd != NULL) {
         _onFastImageLoadEnd(@{});
     }
 }
 
 - (void)setOnFastImageLoad:(RCTDirectEventBlock)onFastImageLoad {
     _onFastImageLoad = onFastImageLoad;
-    if (self.hasCompleted) {
+    if (self.hasCompleted && _onFastImageLoad != NULL) {
         _onFastImageLoad(self.onLoadEvent);
     }
 }
 
 - (void)setOnFastImageError:(RCTDirectEventBlock)onFastImageError {
     _onFastImageError = onFastImageError;
-    if (self.hasErrored) {
+    if (self.hasErrored && _onFastImageError != NULL) {
         _onFastImageError(@{});
     }
 }
 
 - (void)setOnFastImageLoadStart:(RCTDirectEventBlock)onFastImageLoadStart {
-    if (_source && !self.hasSentOnLoadStart) {
+    if (_source && !self.hasSentOnLoadStart && onFastImageLoadStart != NULL) {
         _onFastImageLoadStart = onFastImageLoadStart;
         onFastImageLoadStart(@{});
         self.hasSentOnLoadStart = YES;
